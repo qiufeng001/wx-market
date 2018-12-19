1060fa753d10d0dbee6051c463dc5a892681e7ea
qiufeng001 <2227052723@qq.com>
2018-12-19 14:22:55 +0800

    修改代码

--- wx-market-web/src/main/java/wx/milk/web/controller/admin/TestSoaController.java
+++ wx-market-web/src/main/java/wx/milk/web/controller/admin/TestSoaController.java
@@ -3,13 +3,29 @@ package wx.milk.web.controller.admin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import wx.base.controller.BaseController;
+import wx.base.manager.IManager;
+import wx.base.service.IService;
+import wx.milk.manager.impl.BaseManager;
+import wx.milk.model.Dictions;
 import wx.milk.web.utils.UserUtils;
 import wx.security.User;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-public class TestSoaController {
+@RequestMapping(value = "/soa/*")
+public class TestSoaController extends BaseController<Dictions, String > {
+
+    @Override
+    protected IManager<Dictions, String> getManager() {
+        return null;
+    }
+
+    @Override
+    protected String getTemplateFolder() {
+        return "/file";
+    }
 
     @RequestMapping("/test_soa")
     @ResponseBody
